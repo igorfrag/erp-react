fb06cf0bb33ec7a996b3f2f5875ebffa6ed7355f
Igor Fragoso <igorfragoso3@hotmail.com>
2021-01-27 02:35:21 -0300

    Table Changes

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,22 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-
-let TableContainer = styled.table`
-  border: 1px solid black;
-  text-align: center;
-
-  &.table {
-    grid-area: table;
-  }
-
-  & th {
-    border: 1px solid black;
-    text-align: center;
-  }
-`;
+import { useTable } from 'react-table';
 
 function Table() {
   const [data, setData] = useState([]);
+
   const fetchData = () => {
     fetch(`http://localhost:3080/products/list`)
       .then((res) => res.json())
@@ -27,29 +15,113 @@ function Table() {
     fetchData();
   }, []);
 
-  const tableData = data.map((entry) => {
-    return (
-      <tr key={entry.key}>
-        <td>{entry.key}</td>
-        <td>{entry.name}</td>
-        <td>{entry.price}</td>
-      </tr>
-    );
-  });
+  const tableData = React.useMemo(
+    () => [
+      {
+        key: 1,
+        name: 'Pen',
+        price: 22,
+        stock: 300,
+      },
+      {
+        key: 2,
+        name: 'Pencil',
+        price: 22,
+        stock: 300,
+      },
+      {
+        key: 3,
+        name: 'Notebook',
+        price: 22,
+        stock: 300,
+      },
+    ],
+    []
+  );
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Code',
+        accessor: 'key', // accessor is the "key" in the data
+      },
+      {
+        Header: 'Item',
+        accessor: 'name',
+      },
+
+      {
+        Header: 'Price',
+        accessor: 'price',
+      },
+
+      {
+        Header: 'Stock',
+        accessor: 'stock',
+      },
+    ],
+    []
+  );
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({ columns, data });
 
   return (
-    <TableContainer className='table'>
+    <table
+      {...getTableProps()}
+      style={{ border: 'solid 1px lavender' }}
+      className='table'
+      cellSpacing='0'
+    >
       <thead>
-        <tr>
-          <th>Code</th>
-          <th>Item</th>
-          <th>Price</th>
-          <th>Stock</th>
-        </tr>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th
+                {...column.getHeaderProps()}
+                style={{
+                  borderBottom: 'solid 0.5px lavender',
+                  background: 'ghostwhite',
+                  color: 'black',
+                  fontWeight: 'bold',
+                }}
+              >
+                {column.render('Header')}
+              </th>
+            ))}
+          </tr>
+        ))}
       </thead>
-      <tbody>{tableData}</tbody>
-    </TableContainer>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return (
+                  <td
+                    {...cell.getCellProps()}
+                    style={{
+                      textAlign: 'center',
+                      padding: '10px',
+                      borderBottom: 'solid 0.5px lavender',
+                      background: 'white',
+                    }}
+                  >
+                    {cell.render('Cell')}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 }
-
 export default Table;
