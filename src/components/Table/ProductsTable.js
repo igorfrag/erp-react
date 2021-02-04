cc8ab0e1313241076d6e33e86b9bb377477c7c3a
Igor Fragoso <igorfragoso3@hotmail.com>
2021-02-04 14:56:33 -0300

    Products page grid

--- src/components/Table/ProductsTable.js
+++ src/components/Table/ProductsTable.js
@@ -1,5 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { useTable } from 'react-table';
+import styled from 'styled-components';
+
+const TableTitle = styled.h2`
+  padding: 10px 10px 10px 20px;
+  border-bottom: 1px solid lavender;
+`;
+
+const TableContainer = styled.div`
+  border: 1px solid lavender;
+  border-radius: 10px;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: flex=start;
+`;
 
 function Table() {
   const [data, setData] = useState([]);
@@ -47,56 +62,59 @@ function Table() {
   } = useTable({ columns, data });
 
   return (
-    <table
-      {...getTableProps()}
-      style={{ border: 'solid 1px lavender' }}
-      className='table'
-      cellSpacing='0'
-    >
-      <thead>
-        {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <th
-                {...column.getHeaderProps()}
-                style={{
-                  borderBottom: 'solid 0.5px lavender',
-                  background: 'ghostwhite',
-                  color: 'black',
-                  fontWeight: 'bold',
-                }}
-              >
-                {column.render('Header')}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell) => {
-                return (
-                  <td
-                    {...cell.getCellProps()}
-                    style={{
-                      textAlign: 'center',
-                      padding: '10px',
-                      borderBottom: 'solid 0.5px lavender',
-                      background: 'white',
-                    }}
-                  >
-                    {cell.render('Cell')}
-                  </td>
-                );
-              })}
+    <TableContainer>
+      <TableTitle>Products</TableTitle>
+      <table
+        {...getTableProps()}
+        style={{ border: 'solid 1px lavender' }}
+        className='table'
+        cellSpacing='0'
+      >
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th
+                  {...column.getHeaderProps()}
+                  style={{
+                    borderBottom: 'solid 0.5px lavender',
+                    background: 'ghostwhite',
+                    color: 'black',
+                    fontWeight: 'bold',
+                  }}
+                >
+                  {column.render('Header')}
+                </th>
+              ))}
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td
+                      {...cell.getCellProps()}
+                      style={{
+                        textAlign: 'center',
+                        padding: '10px',
+                        borderBottom: 'solid 0.5px lavender',
+                        background: 'white',
+                      }}
+                    >
+                      {cell.render('Cell')}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </TableContainer>
   );
 }
 export default Table;
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,54 +1,30 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import './pages.css';
 import ProductsTable from '../components/Table/ProductsTable';
 import Button from '../components/Button/Button';
 import Modal from '../components/Modal/Modal';
 import ProductsForm from '../components/Forms/ProductsForm';
-
-const Container = styled.div`
-  margin-left: 10vw;
-  padding: 50px;
-  display: grid;
-  height: 50px;
-  grid-template-columns: repeat(4, 1fr);
-  grid-template-rows: repeat(4, 1fr);
-  gap: 35px 25px;
-  grid-template-areas:
-    '. . . .'
-    'table table graph-two graph-two'
-    'table table sales-order purchase-order';
-  justify-items: center;
-
-  & > Table {
-    width: 100%;
-    height: 100%;
-  }
-`;
-
-const PageTitle = styled.h1`
-  margin-left: 10vw;
-  font-family: Lato;
-  font-size: 30px;
-  margin-left: 11vw;
-  padding-top: 50px;
-`;
 
 function Products() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
-      <PageTitle className='primary'>Products</PageTitle>
-      <Container>
-        <div>
-          <Button handleClick={() => setIsOpen(true)}>Create Product</Button>
-          <Modal open={isOpen} onClose={() => setIsOpen(false)}>
-            <ProductsForm></ProductsForm>
-          </Modal>
+      <div className='container'>
+        Products
+        <div className='grid-container-products'>
+          <div className='ButtonOne'>
+            <Button handleClick={() => setIsOpen(true)}>Create Product</Button>
+            <Modal open={isOpen} onClose={() => setIsOpen(false)}>
+              <ProductsForm></ProductsForm>
+            </Modal>
+          </div>
+          <div className='ProductTable'>
+            <ProductsTable />
+          </div>
         </div>
-
-        <ProductsTable className='table' />
-      </Container>
+      </div>
     </>
   );
 }
